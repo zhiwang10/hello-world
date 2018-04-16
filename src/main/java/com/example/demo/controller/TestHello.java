a996933fc75ce7680e01def5b0fcc354ec9cdbf1
zhiwang10 <695839180@qq.com>
2018-04-16 14:15:43 +0800

    123

--- src/main/java/com/example/demo/controller/TestHello.java
+++ src/main/java/com/example/demo/controller/TestHello.java
@@ -12,7 +12,7 @@ public class TestHello {
         return "hello";
     }
 
-    // 这是一个方法....
+    // 这是一个方法......
     @RequestMapping("/student")
     public Student student(){
         Student s = new Student();
