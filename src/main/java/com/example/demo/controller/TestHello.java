9c51610b58ae67edaedb56d6e88943b7a550616f
zhiwang <695839180.@qq.com>
2018-04-16 11:28:23 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/TestHello.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.entiry.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestHello {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/student")
+    public Student student(){
+        Student s = new Student();
+        s.setAge(1);
+        s.setName("12");
+        return s;
+    }
+
+}
